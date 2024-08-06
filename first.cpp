7d5030224724227d4b19d7b837b29a9a1973cd7e
sunil dhayal <sunildhayal368@gmail.com>
2024-08-06 13:53:11 +0530

    cpp 1st assingment completed

---
+++ first.cpp
@@ -0,0 +1,139 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+
+
+int main(){
+
+// int n ;
+
+// cout<<"enter the value of n \n";
+// cin>>n;
+
+// cout<<n<<"\n";
+
+// cout<<"hello myself sunil dhayal i am from sikar . \n i have completed btech from  govt. engineering collage bikaner\n ";
+
+// int a , b;
+
+// cout<<"enter the value of a\n";
+// cin>>a;
+// cout<<"enter the value of b\n";
+// cin>>b;
+
+// int sum = a + b;
+
+// int psum = abs(sum);
+
+// cout<<"the sum of two number is : \n"<<psum;
+
+
+// average of 5 number 
+
+// int c,d,e,f,g;
+
+// cout<<"enter the value of five number \ns";
+// cin>>c;
+// cin>>d;
+// cin>>e;
+// cin>>f;
+// cin>>g;
+
+// int sum = c+d+e+f+g;
+
+// int avg = sum/5;
+
+// cout<<avg;
+
+
+// swap 2 number ;
+
+
+// int m = 7 ;
+
+// int n = 11;
+
+
+// int temp = m;
+
+
+// m = n;
+
+// n = temp;
+
+
+// cout<<"value of m is :\n "<<m<<"value of n is : \n"<<n;
+
+
+
+// another method
+ 
+// int m = 7 ;
+
+// int n = 11;
+
+
+// m = m + n;
+
+// n  = m - n;
+
+// m = m - n;
+
+
+// cout<<" the value of m : "<<m<<"\n";
+
+// cout<<" the value of n : "<<n<<"\n";
+
+
+// check negative , positive and zero 
+
+
+// int num ;
+
+// cout<<"enter the number :";
+
+// cin>>num;
+
+
+// if (num < 1)
+// {
+//     cout<<"negative number ";
+// }else if (num == 0)
+// {
+//     cout<<"number is zero ";
+// }else{
+//     cout<<"number is positive";
+// }
+
+
+
+
+
+// check even or old
+
+
+ int num ;
+
+ cin>>num;
+
+ 
+ if (num % 2 == 0)
+ {
+    cout<<"number is even";
+ }else
+ {
+    cout<<"number is odd";
+ }
+ 
+ 
+
+
+
+
+
+
+
+return 0;
+}
