ae81e1b8a197a079ce72bc22b814d74e6376688b
sunil dhayal <sunildhayal368@gmail.com>
2024-08-29 20:40:16 +0530

    do some question on array

---
+++ array.cpp
@@ -0,0 +1,75 @@
+
+#include<iostream>
+
+using namespace std;
+
+
+/*Q.1  minimum  element of array */
+
+
+// int main(){
+
+//     int array []  = {1,2,3,4,45,23};
+      
+//      int min = array[5];
+
+//      for (int i = 0; i < 6; i++)
+//      {
+//           if (min > array[i])
+//           {
+//                   min = array[i];
+            
+//           }
+          
+//      }
+
+//      cout<<" "<<min<<endl;
+       
+//     return 0;
+// }
+
+
+
+/* Q.2 find the element in the array */
+
+
+
+// int main(){
+
+//        int arr[] = {1 , 4 , 6 ,7 ,8 ,9 };
+        
+//         int element = 6;
+
+//         bool found = false;
+
+//       for (int i = 0; i < 6; i++)
+//       {
+//           if (element == arr[i])
+//           {
+//                found = true;
+//           }
+          
+//       }
+
+//       if (found)
+//       {
+//          cout<<"element is found";
+//       }  
+//          cout<<"not found";
+      
+    
+//     return 0;
+// }
+
+
+
+/* 2D array */
+  
+  /* Q.3  matrix using 2D array  and multiply    */
+
+int main(){
+       
+       
+
+}
+
