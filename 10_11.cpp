e423555711c559205425a50a0cffe30659f40432
sunil dhayal <sunildhayal368@gmail.com>
2024-11-10 23:10:17 +0530

    categoical data into numerical data

--- 10_11.cpp
+++ 10_11.cpp
@@ -49,5 +49,102 @@ for(auto it : data){
  }
 
 
+
+cout<<"\n"<<"\n"<<"h.w2"<<endl;
+
+
+vector<vector<string>> data1 = {
+          {"female","cancer"},
+          {"female","hypothetinA"},
+          {"male","cough"},
+          {"female","cough"},
+          {"male" , "cancer"},
+          {"male","hypothetinA"}
+};
+
+
+map<string,int> gender_mapping ={
+
+      {"female",0},
+      {"male",1}
+};
+
+map<string,int> disease_mapping = {
+      {"cancer",0},
+      {"hypothetinA",1},
+      {"cough",2}
+};
+
+vector<vector<int>> encodeddata1;
+
+for(auto it:data1){
+         
+      int gender_code = gender_mapping[it[0]];
+      int disease_code = disease_mapping[it[1]];
+
+      encodeddata1.push_back({gender_code,disease_code});
+
+}
+
+
+
+
+
+
+for(auto v1: encodeddata1){
+
+      for(auto it:v1){
+          cout<<it<<" ";
+      }
+      cout<<endl;
+}
+
+cout<<"\n"<<"h.w3"<<endl;
+
+vector<vector<string>> data2 = {
+     {"lion" , "savannah"},
+     {"pengium","antartica"},
+     {"shark","ocen"},
+     {"elephant","savannah"},
+     {"polarbear","antartica"}   
+};
+
+
+
+map<string,int> animal_maping = {
+       {"lion",0},
+       {"pengium",1},
+       {"shark",2},
+       {"elephant",3},
+       {"polorbear",4}
+};
+
+map<string,int> habit_mapping = {
+      {"savannah",0},
+      {"antartica",1},
+      {"ocen",2}
+};
+
+vector<vector<int>> encodeddata2;
+
+for(auto it:data2){
+       int animal_code = animal_maping[it[0]];
+       int habit_code = habit_mapping[it[1]];
+
+           encodeddata2.push_back({animal_code,habit_code});
+}
+
+
+for(auto it: encodeddata2){
+    for(auto i:it){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+
+
+
+
     return 0;
 }
