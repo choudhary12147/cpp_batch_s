c563e245c83719272e9e6bc50409314a32fcfa19
sunil dhayal <sunildhayal368@gmail.com>
2024-11-26 21:42:54 +0530

    map and pair question

--- 11_11.cpp
+++ 11_11.cpp
@@ -1,44 +1,25 @@
-#include<iostream>
-#include<vector>
-#include<map>
-
+#include <iostream>
+#include <vector>
+#include <map>
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-    map<string,string> bestplayers = {
-            {"sunil" , "male"},
-            {"sneha","female"},
-            {"ritika","female"},
-            {"rahul","male"}
+  map<string, string> bestplayers = {
+      {"sunil", "male"},
+      {"sneha", "female"},
+      {"ritika", "female"},
+      {"rahul", "male"}
 
-    };
+  };
 
-    for(auto it:bestplayers){
+  for (auto it : bestplayers)
+  {
 
-        cout<<it.first<<" "<<it.second;
-        cout<<"\n";
-
-    }
-
-
-    // another example
-     
-     cout<<"\n"<<"\n"<<"name"<<" "<<"age"<<" "<<"gender"<<endl;
-
-    map<string, pair<int,string>> bestplayers1 = {
-              
-              {"anil",{23,"male"}},
-              {"sunil",{22,"male"}},
-              {"rahul",{24,"male"}},
-              {"sneha",{23,"female"}}
-
-
-    };
-
-  for(auto it:bestplayers1){
-         cout<<it.first<<" "<<it.second.first<<" "<<it.second.second<<endl;
+    cout << it.first << " " << it.second;
+    cout << "\n";
   }
 
 
@@ -46,9 +27,91 @@ int main(){
 
 
 
+  // another example
+
+  cout << "\n"
+       << "\n"
+       << "name" << " " << "age" << " " << "gender" << endl;
+
+  map<string, pair<int, string>> bestplayers1 = {
+
+      {"anil", {23, "male"}},
+      {"sunil", {22, "male"}},
+      {"rahul", {24, "male"}},
+      {"sneha", {23, "female"}}
+
+  };
+
+  for (auto it : bestplayers1)
+  {
+    cout << it.first << " " << it.second.first << " " << it.second.second << endl;
+  }
 
 
 
 
-    return 0;
+// h.w = 3
+
+cout<<"\n \n \n"<<"H.W = 3"<<endl;
+
+
+struct category
+{
+    int number;
+    string item;
+};
+
+struct person
+{
+     string name;
+     vector<category> categories;
+};
+
+
+vector<person> table;
+
+ person alice;
+
+ alice.name = "Alice";
+
+ alice.categories = { {25, "Apple"},{28,"Orange"}};
+
+ person bob;
+
+ bob.name = "Bob";
+
+ bob.categories = {{26,"Banana"},{34,"Grapess"}};
+
+
+ table.push_back(alice);
+ table.push_back(bob);
+
+
+cout<<"name  "<<"category"<<endl;
+
+ for (int i = 0; i < table.size(); i++)
+ {
+      
+      
+      
+
+ }
+ 
+
+
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+  return 0;
 }
