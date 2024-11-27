ac323e6aad0bc877f4c058bbb1149f1d36b6453a
sunil dhayal <sunildhayal368@gmail.com>
2024-11-27 13:09:19 +0530

    reverse and and frequence count in array using array and structure

---
+++ 27_11.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+// void reverse(int arr[], int n)
+// {
+
+//     int st = 0;
+//     int end = n - 1;
+
+//     while (st < end)
+//     {
+//         int temp = arr[end];
+
+//         arr[end] = arr[st];
+
+//         arr[st] = temp;
+
+//         st++;
+//         end--;
+//     }
+// }
+
+int main()
+{
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // int freq[100] = {0};
+
+    // int arr[] = {40, 15, 45, 15, 20, 25, 30, 35, 40, 45, 20, 30};
+
+    // int n = sizeof(arr) / sizeof(arr[0]);
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     freq[arr[i]]++;
+    // }
+
+    // int m = sizeof(freq) / sizeof(freq[0]);
+
+    // for (int i = 0; i < m; i++)
+    // {
+    //     if (freq[i] > 0)
+    //     {
+    //         cout << i <<":"<<freq[i]<<endl;
+    //     }
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     int arr[] = {19, 18, 17, 16, 15};
+
+    //     int n = sizeof(arr) / sizeof(arr[0]);
+
+    //     reverse(arr, n);
+
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         cout << arr[i] << ",";
+    //     }
+
+    //     cout << endl;
+    //     int arr1[] = {19, 18, 17, 16, 15, 14, 13, 12, 10, 99};
+
+    //     int m = sizeof(arr1) / sizeof(arr1[0]);
+
+    //     reverse(arr1, m / 2);
+
+    //     for (int i = 0; i < m; i++)
+    //     {
+    //         cout << arr1[i] << ",";
+    //     }
+
+    return 0;
+}
