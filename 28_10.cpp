d0309100d32b770e2cbf0569779d7c9ffb91caa6
sunil dhayal <sunildhayal368@gmail.com>
2024-12-01 16:21:31 +0530

    done the assignment

--- 28_10.cpp
+++ 28_10.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include<map>
-#include<vector>
+#include <map>
+#include <vector>
+#include <set>
 using namespace std;
 
 /**
@@ -44,85 +45,172 @@ int main()
         cout << arr[i].name << " " << arr[i].id << " " << arr[i].marks << " " << endl;
     }
 
-  cout<<"\n \n";
+    cout << "\n \n";
 
-  /**Write a structure Address with members city and pincode.
-   *  Then define a Person structure containing name and Address.
-   *  Initialize a Person object and access all its values. */
+    /**Write a structure Address with members city and pincode.
+     *  Then define a Person structure containing name and Address.
+     *  Initialize a Person object and access all its values. */
 
- 
- struct Address
- {
-      string city;
-      int pincode;
- };
+    struct Address
+    {
+        string city;
+        int pincode;
+    };
 
- struct person
- {
-      string name;
-      Address add;
- };
- 
+    struct person
+    {
+        string name;
+        Address add;
+    };
 
-  person arr1[] = {{"sunil",{"sikar",332404}}, {"amit",{"jaipur",344001}}}; 
- 
+    person arr1[] = {{"sunil", {"sikar", 332404}}, {"amit", {"jaipur", 344001}}};
 
- for (int i = 0; i < 2; i++)
- {
-      cout<<arr1[i].name<<" "<<arr1[i].add.city<<" "<<arr1->add.pincode<<" "<<endl;
- }
+    for (int i = 0; i < 2; i++)
+    {
+        cout << arr1[i].name << " " << arr1[i].add.city << " " << arr1->add.pincode << " " << endl;
+    }
+
+    /** Create a map<int, string> to store student roll numbers and their names. Initialize it with at least 3 entries.:
+   Perform the following on a map<int, string>:
+   Insert a new pair.
+   Find an element by its key.
+   Erase an element */
+
+    map<int, string> student = {
+
+        {2345, "sunil"},
+        {7895, "amit"},
+        {7856, "rahul"}
+
+    };
+
+    for (const auto &pair : student)
+    {
+        cout << pair.first << " " << pair.second << endl;
+    }
+
+    student.insert({8769, "anil"});
+
+    for (const auto &pair : student)
+    {
+        cout << pair.first << " " << pair.second << endl;
+    }
+
+    cout << student[2345] << endl;
+
+    student.erase(2345);
+
+    for (const auto &pair : student)
+    {
+        cout << pair.first << " " << pair.second << endl;
+    }
+
+    cout << "\n \n";
+
+    /*
+     Create a vector<int> and initialize it with the values {10, 20, 30, 40, 50}.:
+  Perform the following operations on the vector:
+  Add a new element at the end.
+  Insert an element at the second position.
+  Erase the third element.
+    */
+
+    vector<int> vec = {10, 20, 30, 40, 50};
+
+    // 1
+
+    vec.push_back(60);
+    for (auto it : vec)
+    {
+        cout << it << ",";
+    }
+    cout << endl;
+
+    // 2
+
+    vec.insert(vec.begin() + 1, 45);
+
+    for (auto it : vec)
+    {
+        cout << it << ",";
+    }
+    cout << endl;
+
+    // 3
+
+    vec.erase(vec.begin() + 2);
+
+    for (auto it : vec)
+    {
+        cout << it << ",";
+    }
+    cout << "\n \n \n";
+
+    /*
+    Create a vector<pair<int, string>> to store product IDs and their names. Initialize and display the elements.
+    */
+
+    vector<pair<int, string>> product = {
+
+        {2314, "sunil"},
+        {5636, "amit"},
+        {7578, "anil"}
+
+    };
+
+    cout << "id  " << "name  " << endl;
+
+    for (int i = 0; i < product.size(); i++)
+    {
+        cout << product[i].first << "  " << product[i].second << endl;
+    }
+
+    // Create a set<int> and initialize it with the values {15, 10, 20, 10, 5}. Print the elements of the set.:
+
+    set<int> s = {15, 10, 20, 10, 5};
+
+       
+      for(auto it:s){
+           cout<<it<<",";
+      }
+
+  cout<<"\n";
+      /*
+        Insert a new value.
+        
+      */
+       
 
 
- /** Create a map<int, string> to store student roll numbers and their names. Initialize it with at least 3 entries.:
-Perform the following on a map<int, string>:
-Insert a new pair.
-Find an element by its key.
-Erase an element */
- 
- 
- map<int,string> student = {
-                    
-                    {2345,"sunil"},
-                    {7895,"amit"},
-                    {7856,"rahul"}
+      s.insert(45);
 
- };
-
- for(const auto &pair : student){
-     cout<<pair.first<<" "<<pair.second<<endl;
- }
-
-student.insert({8769,"anil"});
-
-for(const auto &pair : student){
-     cout<<pair.first<<" "<<pair.second<<endl;
- }
+    for(auto it:s){
+           cout<<it<<",";
+        }
 
 
- cout<<student[2345]<<endl;
-
- student.erase(2345);
-
-for(const auto &pair : student){
-     cout<<pair.first<<" "<<pair.second<<endl;
- }
-
-  
-
-  cout<<"\n \n";
-
-  /*
-   Create a vector<int> and initialize it with the values {10, 20, 30, 40, 50}.:
-Perform the following operations on the vector:
-Add a new element at the end.
-Insert an element at the second position.
-Erase the third element.
-  */
+/**
+   Find an element.
+ */
 
 
+      int toFind = 10;
 
+    if (s.find(toFind) != s.end()) {
+          cout<<"found"<<endl;
+    } else {
+         cout<<"not found"<<endl;
+    }
 
+/*
+ Remove an element.
+*/
 
+  s.erase(5);
+
+  for(auto it:s){
+           cout<<it<<",";
+        }
 
 
 
